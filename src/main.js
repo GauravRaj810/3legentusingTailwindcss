55c99038af15913f25eb90c21d0b61b58624659d
GauravRaj810 <gauravskg810@gmail.com>
2025-03-20 18:26:16 +0530

    status : updated upto arrival slider , responsivers not comp of grid section of feature part

--- src/main.js
+++ src/main.js
@@ -21,6 +21,10 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     768: {
       navigation: {
@@ -45,3 +49,64 @@ var swiper = new Swiper(".mySwiper", {
 //       video.pause(); // Pause video if playing
 //     }
 //   });
+
+
+//  ************** cross hamburger working  ***********
+
+const crossHamburgerBtn = document.querySelector("#cross-hamburger-btn");
+const mobileMenu = document.querySelector("#mobile-menu");
+const heroSection = document.querySelector(".hero-section");
+const menuItemSection = document.querySelector(".mobile-menu-item")
+// Set initial state
+function showMenu() {
+  mobileMenu.classList.remove("hidden");
+  crossHamburgerBtn.classList.add("hidden");
+  heroSection.classList.remove("hidden");
+  menuItemSection.classList.add("hidden");
+}
+function showHamburger() {
+  mobileMenu.classList.add("hidden");
+  crossHamburgerBtn.classList.remove("hidden");
+  heroSection.classList.add("hidden");
+  menuItemSection.classList.remove("hidden");
+}
+// Initial state - showing the menu bars icon first
+showMenu();
+// Add event listeners
+mobileMenu.addEventListener("click", showHamburger);
+crossHamburgerBtn.addEventListener("click", showMenu);
+
+
+
+// ************ arrival section swiper   ******************************
+var featureSwiper = new Swiper(".featureSwiper", {
+  grabCursor: true,           // Enable grab cursor when hovering over the swiper
+  slidesPerView: "auto",      // Set slides per view to 'auto' to respect the width of each slide
+  spaceBetween: 10,           // Space between slides
+  pagination: {
+    el: ".swiper-pagination",  // Select pagination container
+    type: "progressbar",      // Set pagination type to progress bar
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: "auto",   // Keep slides' width as 'auto'
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: "auto",   // Keep slides' width as 'auto'
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4.2,      // Keep slides' width as 'auto'
+      spaceBetween: 15,
+    },
+  },
+});
+
+
+
+
+
+
+
+
